ab7750194cd7865aa243e5baed17fd69a639b6fa
Chugaev Oleg <ochugaev@bloomberg.net>
2018-02-15 16:00:21 -0500

    first commit

---
+++ AppServiceHelloWorldMVC/AppServiceHelloWorldMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppServiceHelloWorldMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
